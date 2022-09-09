221338098589b3b03773357c4fbfaa455a625f75
Leonhard Francesco Sensi <frank@Leonhards-MacBook-Pro.local>
2022-09-09 18:02:39 +0200

    results123

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -7,7 +7,7 @@ class SearchResults extends React.Component {
     render() {
         return(
             <div className="SearchResults">
-                <h2>Results</h2>
+                <h2>Results123</h2>
                 <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false} />
             </div>
         )
